ec67d993ea06242b5c086e7f1c30551a0bc47825
eduardoc9 <eduardo.calderon@troiatec.com>
2023-01-24 20:23:41 -0600

    colored table

--- Kevyn_Reporte.py
+++ Kevyn_Reporte.py
@@ -128,25 +128,28 @@ def generate_pptx(prs):
             index=plot_df.index, columns=plot_df.columns
         )
                 
-        colors = ['rgb(239, 243, 255)', 'rgb(189, 215, 231)', 'rgb(107, 174, 214)']
-        data = {'Year' : [2010, 2011, 2012, 2013, 2014], 'Color' : colors}
+        colors = ['rgb(235, 222, 52)', 'rgb(235, 222, 52)', 'rgb(235, 222, 52)', 'rgb(255, 255, 255)', 'rgb(255, 255, 255)', 'rgb(255, 255, 255)']
+        data = {}
+        for col in keyword_df.columns:
+            data[col] = keyword_df[col].to_numpy()
+        data['Color'] = colors
+        print(data)
         df = pd.DataFrame(data)
-
         fig = go.Figure(data=[go.Table(
                                 header=dict(
-                                values=["Color", "<b>YEAR</b>"],
-                                line_color='white', fill_color='white',
+                                values=[col for col in df if col != 'Color'],
+                                line_color='black', fill_color='white',
                                 align='center', font=dict(color='black', size=12)
                             ),
                             cells=dict(
-                                values=[df.Color, df.Year],
-                                line_color=[df.Color]
+                                values=[df[col].values for col in df  if col != 'Color'],
+                                line_color=['rgb(0, 0, 0)'] * len(colors)
                                 , fill_color=[df.Color]
                                 , align='center'
                                 , font=dict(color='black', size=11)
                             ))
                         ])
-        #fig.show()  
+        #fig.show()
         print("figura")
         st.plotly_chart(fig)
         fig.write_image('pruebita.png')
